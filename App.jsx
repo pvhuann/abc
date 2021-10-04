12effc0d16711bb276c8bd14ce795266391fc71b
eidogs <91258928+eidogs@users.noreply.github.com>
2021-10-04 13:14:16 +0200

    Init
    Init

--- App.jsx
+++ App.jsx
@@ -5,8 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" backgroundColor={theme.colors.primary} />
     </View>
   );
 }
@@ -14,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: theme.colors.white,
     justifyContent: 'center',
+    alignItems: 'center'
   },
 });
